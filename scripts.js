30f17f20cde662bd8cc75a97c80d19bcd1d27dac
Tiago Pereira <71046422+tiagocpereira@users.noreply.github.com>
2023-03-17 19:11:25 -0300

    commit
    script

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTasKButton = document.querySelector(".new-task-button");
+const tasksContainer = document.querySelector('.tasks-container');
 
 const validateInput = () => inputElement.ariaValueMax.trim().length > 0;
 
@@ -10,15 +11,23 @@ const handleAddTask = () => {
         return inputElement.classList.add("error");
     }
 
-    const taskItemContainer = document.createElement('div')
-    taskItemContainer.classList.add('task-item')
+    const taskItemContainer = document.createElement('div');
+    taskItemContainer.classList.add('task-item');
 
-    const taskContent = document.createElement('p')
+    const taskContent = document.createElement('p');
     taskContent.innerText = inputElement.value;
 
-    const deleteItem = document.createElement('i')
-    deleteItem.classList.add('fa-regular')
-    deleteItem.classList.add('fa-trash-can')
+    const deleteItem = document.createElement('i');
+    deleteItem.classList.add('fa-regular');
+    deleteItem.classList.add('fa-trash-can');
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+
 };
 
 const handleInputChange = () => {
